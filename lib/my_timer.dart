d877c8466255269f00f750c623b0d47f2c553e0e
dabuzida <dabuzida@gmail.com>
2024-02-27 09:21:03 +0900

    test

---
+++ lib/my_timer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyTimer extends StatefulWidget {
+  const MyTimer({super.key});
+
+  @override
+  State<MyTimer> createState() => _MyTimerState();
+}
+
+class _MyTimerState extends State<MyTimer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
